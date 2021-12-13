bd1dc9503b9a215f3946035c90012cca3c1aa6c8
kosukenemy <kosukenemy@gmail.com>
2021-12-13 23:39:49 +0900

    update: genreで検索した際の映画APIを呼び出す処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { getMovieGenres } from '../src/tmdbAPI';
+import { getGenresQueryMovie } from '../src/tmdbAPI';
+
+type initialGenre = {
+  [key: string]: any;
+}
 
 type Genre = {
   id: string;
   name: string;
 }
 
+type GenreMovie = {
+  id: string;
+  overview: string;
+  original_title: string;
+  backdrop_path: string;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+  likedCount?: number;
+}
 
 function App() {
+  const initialGenreName:initialGenre = { Action: 28 };
+  const initialGenreId:initialGenre = { type: initialGenreName.initialGenreId };
   const [genres, setGenres] = useState<Genre[]>([]);
+  const [genresMovie, setGenreMovie] = useState<GenreMovie[]>([]);
   const fetchAPI = async() => {
-    setGenres(await getMovieGenres())
+    setGenres(await getMovieGenres());
+    setGenreMovie( await getGenresQueryMovie(initialGenreId.type));
   }
 
-  useEffect(() => { fetchAPI() }, [])
+  console.log(genresMovie)
+
+  useEffect(() => { fetchAPI() }, []);
+
+
+  const handleGenreType = async(genreId:string) => {
+    initialGenreId.type = genreId;
+    return fetchAPI();
+  }
 
   return (
     <div className="App">
       <div className="sideBar">
+        <ul className="sideMenu">
         {genres.map((genre, index) => {
           return (
-            <ul key={index}>
-              <li><button id={genre.id}>{genre.name}</button></li>
-            </ul>
+              <li key={index} className="sideMenu-list">
+                <button onClick={ (event) => handleGenreType(event.currentTarget.id) } 
+                id={genre.id}>{genre.name}</button>
+              </li>
           )
         })}
+        </ul>
       </div>
       <div className="main">main</div>
     </div>
--- src/tmdbAPI/index.tsx
+++ src/tmdbAPI/index.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
-
-
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const APIQuery = "?api_key=";
 const GenreQuery = "&with_genres=";
 const genreUrl = `https://api.themoviedb.org/3/genre/movie/list${APIQuery}${API_KEY}`;
+const movieWithGenreURL = "https://api.themoviedb.org/3/discover/movie" + APIQuery + API_KEY + GenreQuery;
+
 
 export const getMovieGenres = async() => {
     try {
@@ -23,3 +23,28 @@ export const getMovieGenres = async() => {
     } catch (error) { }
 }
 
+export const getGenresQueryMovie = async(genreId:string) => {
+    try {
+        const { data } = await axios.get(`${movieWithGenreURL}${genreId}`, {
+            params: {
+                api_key: API_KEY,
+                language: 'en',
+                page: 1
+            }
+        })
+        const modifiedData = data['results'].map((g:any) => ({
+            id: g['id'],
+            original_title: g['original_title'],
+            title: g['title'],
+            overview: g['overview'],
+            popularity: g['popularity'],
+            poster_path: g['poster_path'],
+            backdrop_path: g['backdrop_path'],
+            genre_ids: g['genre_ids'],
+            release_date: g['release_date'],
+            vote_average: g['vote_average'],
+            vote_count: g['vote_count']
+        }))
+        return modifiedData;
+    } catch (error) { }
+} 
